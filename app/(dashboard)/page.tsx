ec02dd9cd4adc886c071c0ee7d982195ef6f077e
Tasketeer <nico@contentking.de>
2025-09-22 13:17:42 +0200

    Homepage hero: make team image responsive (object-contain, auto height, sizes attr)

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1272,13 +1272,14 @@ export default function HomePage() {
               </div>
             </div>
             <div className="mt-12 relative sm:max-w-lg sm:mx-auto lg:mt-0 lg:max-w-none lg:mx-0 lg:col-span-6 lg:flex lg:items-center">
-              <div className="relative w-full h-96 lg:h-auto">
+              <div className="relative w-full">
                 <Image
                   src="/lingoletics-team.png"
                   alt="Lingoletics Team"
                   width={600}
                   height={400}
-                  className="w-full h-full object-cover rounded-lg shadow-lg"
+                  sizes="(max-width: 1024px) 100vw, 600px"
+                  className="w-full h-auto object-contain rounded-lg shadow-lg"
                   priority
                 />
               </div>
